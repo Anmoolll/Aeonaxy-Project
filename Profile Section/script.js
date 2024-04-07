edfaa21815ba7c567cbf7e54ea6d74e551092ed5
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-04-07 12:26:42 +0530

    added return button functionality and button disabled or enabled

--- Profile Section/script.js
+++ Profile Section/script.js
@@ -1,6 +1,22 @@
 const profilePic = document.querySelector("#Profile-pic");
 const inputFile = document.querySelector("#input-file");
+const inputLocation = document.querySelector("#location");
+const button = document.querySelector("#submit");
 
 inputFile.onchange = () => {
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
 }
+
+inputLocation.addEventListener("input", function(){
+    if (inputLocation.value.trim() !== '') {
+        button.removeAttribute('disabled');
+    } else {
+        button.setAttribute('disabled', 'disabled');
+    }
+})
+
+inputLocation.addEventListener("keyup", function(e){
+    if (e.key === 'Enter') {
+        button.click();
+    }
+})
