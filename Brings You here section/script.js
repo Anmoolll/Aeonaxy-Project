69a9c611ba05522fd7dbee33a1647f51f29f90ea
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-04-09 00:51:11 +0530

    Added Transitions

--- Brings You here section/script.js
+++ Brings You here section/script.js
@@ -5,26 +5,36 @@ const divWork = document.querySelector(".work");
 const divHire = document.querySelector(".hire");
 const divInspo = document.querySelector(".inspo");
 const radio = document.querySelector('input[type="radio"]');
-const message = document.querySelector(".hidden");
+const messageWork = document.querySelector("#msgWork");
+const messageHire= document.querySelector("#msgHire");
+const messageInspo = document.querySelector("#msgInspo");
+const message = document.querySelector("#message");
 const card = document.querySelector(".card");
 const button = document.querySelector(".Finish")
+
+
 function changeBorderColorWork(divWork){
     divWork.style.border = "3px solid #ea4b8b"
     radioWork.checked=true;
-    message.style.display = 'block'
+    message.style.display = 'block';
+    messageWork.style.display = 'block'
     button.removeAttribute('disabled');
+    divWork.querySelector("img").style.marginTop = '-40%';
 }
 
 function changeBorderColorHire(divHire){
     divHire.style.border = "3px solid #ea4b8b"
     radioHire.checked=true;
-    message.style.display = 'block'
+    messageHire.style.display = 'block'
     button.removeAttribute('disabled');
+    divHire.querySelector("img").style.marginTop = '-40%';
 }
 
 function changeBorderColorInspo(divInspo){
     divInspo.style.border = "3px solid #ea4b8b"
     radioInspo.checked=true;
-    message.style.display = 'block'
+    messageInspo.style.display = 'block'
     button.removeAttribute('disabled');
+    divInspo.querySelector("img").style.marginTop = '-40%';
+    // divWork.querySelector('img').style.transition = 'margin-top 1.3s ease';
 }
