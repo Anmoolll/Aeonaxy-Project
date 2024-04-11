e0494e5b51710a7a767fceffbfb6164f84ad321e
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-04-11 16:31:12 +0530

    final step in the verification page

--- Verification/script.js
+++ Verification/script.js
@@ -1,5 +1,7 @@
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 
+
+
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
@@ -9,3 +11,19 @@ function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
+
+// Retrieve user data from local storage
+var user_data = JSON.parse(localStorage.getItem("user")) || [];
+
+// Extract email addresses
+var emailAddresses = user_data.map(function(user) {
+  return user.email;
+});
+
+// Get the p tag element
+var emailDisplay = document.querySelector(".show-mail");
+
+// Display email addresses in the p tag
+emailDisplay.textContent = emailAddresses.join(", ");
+
